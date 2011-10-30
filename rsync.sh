aad4f24d74e60052eb1bd6ba159089e4e8e187aa
ndruger <snowfullnight@gmail.com>
2011-10-31 00:26:06 +0900

    a

--- rsync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#rsync -zr --delete -e ssh --exclude '*.oni' --exclude='*.git*' * snow@www2.syspri.org:/var/www/html/test/kinect-modeling-tool
-rsync -zr --delete -e ssh --exclude '*.oni' --exclude='*.git*' * snow@www.syspri.org:/var/www/html/test/kinect-modeling-tool
-
